4261010613fc9124d114be9dcef64b595396de3a
ij0319 <119471118+ij0319@users.noreply.github.com>
2024-09-09 09:26:15 -0300

    Add files via upload

---
+++ Cor/script.js
@@ -0,0 +1,15 @@
+var x;
+console.log(typeof x)
+
+var x = "JAvaScript";
+console.log(typeof x)
+
+var x = true;
+console.log(typeof x)
+
+var x = ["cachorro", "gato", "leão"];
+console.log(typeof x)
+
+var str = new String("Olá 'Harry'")
+console.log(str)
+console.log(typeof str)
